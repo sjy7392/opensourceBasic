b527d07d1886f1b3f88abd305b0674978e891098
Jiyeon <sjy7392@naver.com>
2023-05-29 23:22:54 +0900

    함수형, cv 정의

---
+++ video_app.py
@@ -0,0 +1,10 @@
+import cv2
+
+
+class VideoApp:
+    def __init__(self, window, window_title, video_source=0):
+        self.window = window
+        self.window.title(window_title)
+        self.video_source = video_source
+        self.vid = cv2.VideoCapture(video_source)
+        self.window.protocol('WM_DELETE_WINDOW', self.destroy)
