e8bb145647a43b76e7a7b7e1977e3410cd8406f7
Jiyeon <sjy7392@naver.com>
2023-05-29 22:16:34 +0900

    메인파일 생성

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import threading
 import argparse
+import cv2
+import tkinter as tk
 
-parser=argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
 parser.add_argument('--image')
 
-args=parser.parse_args()
+args = parser.parse_args()
