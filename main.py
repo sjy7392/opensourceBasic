b153eded4d736666e2a438d0a11831186012f855
Jiyeon <sjy7392@naver.com>
2023-05-29 22:20:24 +0900

    모델 업뎃

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import argparse
 import cv2
 import tkinter as tk
 
+face_proto = "opencv_face_detector.pbtxt"
+face_model = "opencv_face_detector_uint8.pb"
+age_proto = "age_deploy.prototxt"
+age_model = "age_net.caffemodel"
+gender_proto = "gender_deploy.prototxt"
+gender_model = "gender_net.caffemodel"
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--image')
 
