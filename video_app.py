2879098e3c1bf6f23ca18be41a14873208c85413
Jiyeon <sjy7392@naver.com>
2023-05-29 23:38:28 +0900

    canvas 사용

--- video_app.py
+++ video_app.py
@@ -1,10 +1,16 @@
+import tkinter as tk
 import cv2
+from PIL import Image, ImageTk
 
 
 class VideoApp:
-    def __init__(self, window, window_title, video_source=0):
+    def __init__(self, window, window_title, video_source=0, face_net=None, age_net=None, gender_net=None):
         self.window = window
         self.window.title(window_title)
         self.video_source = video_source
         self.vid = cv2.VideoCapture(video_source)
         self.window.protocol('WM_DELETE_WINDOW', self.destroy)
+
+        self.canvas = tk.Canvas(window, width=self.vid.get(
+            cv2.CAP_PROP_FRAME_WIDTH), height=self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.canvas.pack()
