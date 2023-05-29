a081783fd06b965c82b633991430d82b5d93b292
Jiyeon <sjy7392@naver.com>
2023-05-29 23:50:45 +0900

    함수 밖으로 모델 빼냄

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ import argparse
 import cv2
 import tkinter as tk
 
+# Global variables
+face_proto = "opencv_face_detector.pbtxt"
+face_model = "opencv_face_detector_uint8.pb"
+age_proto = "age_deploy.prototxt"
+age_model = "age_net.caffemodel"
+gender_proto = "gender_deploy.prototxt"
+gender_model = "gender_net.caffemodel"
+
+# Initialize the models
+face_net = cv2.dnn.readNet(face_model, face_proto)
+age_net = cv2.dnn.readNet(age_model, age_proto)
+gender_net = cv2.dnn.readNet(gender_model, gender_proto)
+
 
 def main():
-    # Global variables
-    face_proto = "opencv_face_detector.pbtxt"
-    face_model = "opencv_face_detector_uint8.pb"
-    age_proto = "age_deploy.prototxt"
-    age_model = "age_net.caffemodel"
-    gender_proto = "gender_deploy.prototxt"
-    gender_model = "gender_net.caffemodel"
-
-    # Initialize the models
-    face_net = cv2.dnn.readNet(face_model, face_proto)
-    age_net = cv2.dnn.readNet(age_model, age_proto)
-    gender_net = cv2.dnn.readNet(gender_model, gender_proto)
-
     # Parse the arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--image')
     args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
