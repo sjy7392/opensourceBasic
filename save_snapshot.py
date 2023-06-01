950e9657d6d42cc0f1512baee23531eadeee979c
Jiyeon <sjy7392@naver.com>
2023-06-01 15:49:22 +0900

    snapshot 함수 변경

--- save_snapshot.py
+++ save_snapshot.py
@@ -4,9 +4,13 @@ import time
 
 
 def save_snapshot(result_img, data):
-    cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") +
-                ".jpg", cv2.cvtColor(result_img, cv2.COLOR_RGB2BGR))
-    with open('output.csv', 'w', newline='') as f:
+    timestamp = time.strftime("%d-%m-%Y-%H-%M-%S")
+    image_path = f"frame-{timestamp}.jpg"
+    csv_path = f"output-{timestamp}.csv"
+
+    cv2.imwrite(image_path, cv2.cvtColor(result_img, cv2.COLOR_RGB2BGR))
+
+    with open(csv_path, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(["Gender", "Age"])
         writer.writerows(data)
