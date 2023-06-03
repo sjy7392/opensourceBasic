47ef84441ad7a5059c5071d0763d9cb0de1388c6
Jiyeon <sjy7392@naver.com>
2023-06-04 02:12:35 +0900

    함수 안으로 수정

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import threading
 import argparse
 import cv2
-from video_app import VideoApp
 import tkinter as tk
+from video_app import VideoApp
 
 # Global variables
 face_proto = "opencv_face_detector.pbtxt"
@@ -12,25 +11,31 @@ age_model = "age_net.caffemodel"
 gender_proto = "gender_deploy.prototxt"
 gender_model = "gender_net.caffemodel"
 
-# Initialize the models
-face_net = cv2.dnn.readNet(face_model, face_proto)
-age_net = cv2.dnn.readNet(age_model, age_proto)
-gender_net = cv2.dnn.readNet(gender_model, gender_proto)
-
 
 def main():
     # Parse the arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image')
+    parser.add_argument('--image', help='Path to the image file')
+    parser.add_argument('--video', help='Path to the video file')
     args = parser.parse_args()
 
     # If an image file is given, use it as the video source. Otherwise, use the webcam.
-    video_source = args.image if args.image else 0
+    if args.image:
+        video_source = args.image
+    elif args.video:
+        video_source = args.video
+    else:
+        video_source = 0
 
     # Start the main loop in a separate thread
+    face_net = cv2.dnn.readNet(face_model, face_proto)
+    age_net = cv2.dnn.readNet(age_model, age_proto)
+    gender_net = cv2.dnn.readNet(gender_model, gender_proto)
+
     app = VideoApp(tk.Tk(), "Tkinter and OpenCV",
                    video_source, face_net, age_net, gender_net)
     app.window.mainloop()
+
 
 if __name__ == "__main__":
     main()
