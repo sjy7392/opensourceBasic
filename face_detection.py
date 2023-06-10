4ce70f4ed387226a23ddf5d90634a1b79475a1e7
Jiyeon <sjy7392@naver.com>
2023-06-10 23:23:38 +0900

    process_frame5

--- face_detection.py
+++ face_detection.py
@@ -47,3 +47,7 @@ def process_frame(face_net, age_net, gender_net, frame, padding=20):
         gender_net.setInput(blob)
         gender_preds = gender_net.forward()
         gender = gender_list[gender_preds[0].argmax()]
+
+        age_net.setInput(blob)
+        age_preds = age_net.forward()
+        age = age_list[age_preds[0].argmax()]
