d83bd11fd76e571f2bae652e7f50d23a06456199
Jiyeon <sjy7392@naver.com>
2023-06-10 23:15:28 +0900

    highlight_face

--- face_detection.py
+++ face_detection.py
@@ -3,3 +3,11 @@ import cv2
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 age_list = ['(0-2)', '(4-6)', '(8-12)', '(15-20)',
             '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+
+
+def highlight_face(net, frame, conf_threshold=0.7):
+    frame_opencv_dnn = frame.copy()
+    frame_height = frame_opencv_dnn.shape[0]
+    frame_width = frame_opencv_dnn.shape[1]
+    blob = cv2.dnn.blobFromImage(frame_opencv_dnn, 1.0, (300, 300), [
+                                 104, 117, 123], True, False)
